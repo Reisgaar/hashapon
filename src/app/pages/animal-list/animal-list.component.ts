4f004479cf22bcd3136bb634562e641f2c846e96
Asiergaar <asier.garcia@nexxyolabs.com>
2022-02-23 12:33:58 +0100

    create animals list

--- src/app/pages/animal-list/animal-list.component.ts
+++ src/app/pages/animal-list/animal-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-animal-list',
@@ -7,9 +8,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnimalListComponent implements OnInit {
 
-  constructor() { }
+  egg: Array<any> = [
+    {type: 'birds'},
+    {type: 'canines'},
+    {type: 'felines'},
+    {type: 'marsupials'},
+    {type: 'primates'},
+    {type: 'reptiles'},
+    {type: 'rodents'},
+    {type: 'ursids'}
+  ];
+
+  baby: Array<any> = [
+    {type: 'baby'},
+    {type: 'baby'},
+    {type: 'baby'},
+    {type: 'baby'},
+  ];
+
+  young: Array<any> = [
+    {type: 'young'},
+    {type: 'young'},
+    {type: 'young'},
+    {type: 'young'},
+    {type: 'young'},
+    {type: 'young'}
+  ];
+
+  adult: Array<any> = [
+    {type: 'adult'},
+    {type: 'adult'},
+    {type: 'adult'},
+    {type: 'adult'},
+    {type: 'adult'},
+    {type: 'adult'},
+    {type: 'adult'},
+    {type: 'adult'},
+    {type: 'adult'},
+    {type: 'adult'},
+    {type: 'adult'},
+    {type: 'adult'}
+  ];
+
+  constructor(
+    public dialogRef: MatDialogRef<AnimalListComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
 }
