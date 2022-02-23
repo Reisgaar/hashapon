9065495330f85146103b5477caa6a2dc3c7fd34d
Asiergaar <asier.garcia@nexxyolabs.com>
2022-02-23 14:11:16 +0100

    set eggslider

--- src/app/pages/animal-list/animal-list.component.ts
+++ src/app/pages/animal-list/animal-list.component.ts
@@ -8,47 +8,10 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class AnimalListComponent implements OnInit {
 
-  egg: Array<any> = [
-    {type: 'birds'},
-    {type: 'canines'},
-    {type: 'felines'},
-    {type: 'marsupials'},
-    {type: 'primates'},
-    {type: 'reptiles'},
-    {type: 'rodents'},
-    {type: 'ursids'}
-  ];
-
-  baby: Array<any> = [
-    {type: 'baby'},
-    {type: 'baby'},
-    {type: 'baby'},
-    {type: 'baby'},
-  ];
-
-  young: Array<any> = [
-    {type: 'young'},
-    {type: 'young'},
-    {type: 'young'},
-    {type: 'young'},
-    {type: 'young'},
-    {type: 'young'}
-  ];
-
-  adult: Array<any> = [
-    {type: 'adult'},
-    {type: 'adult'},
-    {type: 'adult'},
-    {type: 'adult'},
-    {type: 'adult'},
-    {type: 'adult'},
-    {type: 'adult'},
-    {type: 'adult'},
-    {type: 'adult'},
-    {type: 'adult'},
-    {type: 'adult'},
-    {type: 'adult'}
-  ];
+  egg: Array<any> = [ {type: 'birds'}, {type: 'canines'}, {type: 'felines'}, {type: 'marsupials'}, {type: 'primates'}, {type: 'reptiles'}, {type: 'rodents'}, {type: 'ursids'} ];
+  baby: Array<any> = [ {type: 'baby'}, {type: 'baby'}, {type: 'baby'}, {type: 'baby'} ];
+  young: Array<any> = [ {type: 'young'}, {type: 'young'}, {type: 'young'}, {type: 'young'}, {type: 'young'}, {type: 'young'} ];
+  adult: Array<any> = [ {type: 'adult'}, {type: 'adult'}, {type: 'adult'}, {type: 'adult'}, {type: 'adult'}, {type: 'adult'}, {type: 'adult'}, {type: 'adult'}, {type: 'adult'}, {type: 'adult'}, {type: 'adult'}, {type: 'adult'} ];
 
   constructor(
     public dialogRef: MatDialogRef<AnimalListComponent>,
@@ -60,6 +23,21 @@ export class AnimalListComponent implements OnInit {
 
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  changeTab(event: any): void {
+    document.getElementById('tab-eggs').classList.remove('active');
+    document.getElementById('tab-babies').classList.remove('active');
+    document.getElementById('tab-youngs').classList.remove('active');
+    document.getElementById('tab-adults').classList.remove('active');
+
+    document.getElementById('cat-eggs').classList.add('hidden');
+    document.getElementById('cat-babies').classList.add('hidden');
+    document.getElementById('cat-youngs').classList.add('hidden');
+    document.getElementById('cat-adults').classList.add('hidden');
+
+    document.getElementById(event.target.id).classList.add('active');
+    document.getElementById(event.target.id.replace('tab-', 'cat-')).classList.remove('hidden');
   }
 
 }
