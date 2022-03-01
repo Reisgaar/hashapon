a8220b326f7ad06859c88c0c7e00292fd97c3aa9
Asiergaar <asier.garcia@nexxyolabs.com>
2022-03-01 11:37:55 +0100

    create gym component base

--- src/app/pages/gym/gym.component.ts
+++ src/app/pages/gym/gym.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-gym',
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GymComponent implements OnInit {
 
+  machineHeightIsOccupied: boolean = false;
+  machineBoxingIsOccupied: boolean = false;
+  machineRunningIsOccupied: boolean = false;
+  actualPosition: number = 5;
+
+  @HostListener('window:resize', ['$event'])
+
+  handleResize(event: any) {
+    if (event.target.innerWidth > 1000) {
+      const firstEgg = document.getElementById('first-machine') as HTMLElement;
+      this.actualPosition = 0;
+      firstEgg.style.marginLeft = this.actualPosition + 'vw';
+    }
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public moveSlider(next: boolean): void {
+    const firstBaby = document.getElementById('first-machine') as HTMLElement;
+    // Set actual position
+    if (next) {
+      this.actualPosition = this.actualPosition - 100;
+    } else {
+      this.actualPosition = this.actualPosition + 100;
+    }
+    // Check if first or last
+    if (this.actualPosition === (-295)) {
+      this.actualPosition = 5;
+    } else if (this.actualPosition === 100) {
+      this.actualPosition = -100 * (2 - 1);
+    }
+    // Update position
+    firstBaby.style.marginLeft = this.actualPosition + 'vw';
   }
 
 }
