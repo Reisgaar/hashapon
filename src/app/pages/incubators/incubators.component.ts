52dbf313cf466535f9cabb160d4ba63bc69c396a
Asiergaar <asier.garcia@nexxyolabs.com>
2022-03-01 15:12:08 +0100

    create incubators component

--- src/app/pages/incubators/incubators.component.ts
+++ src/app/pages/incubators/incubators.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-incubators',
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IncubatorsComponent implements OnInit {
 
+  machineHeightAnimal: any;
+  machineBoxingAnimal: any;
+  machineRunningAnimal: any;
+  actualPosition: number = 5;
+
+  @HostListener('window:resize', ['$event'])
+
+  handleResize(event: any): void {
+    if (event.target.innerWidth > 1000) {
+      const firstEgg = document.getElementById('first-incubator') as HTMLElement;
+      this.actualPosition = 0;
+      firstEgg.style.marginLeft = this.actualPosition + 'vw';
+    }
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public moveSlider(next: boolean): void {
+    const firstBaby = document.getElementById('first-incubator') as HTMLElement;
+    // Set actual position
+    if (next) {
+      this.actualPosition = this.actualPosition - 100;
+    } else {
+      this.actualPosition = this.actualPosition + 100;
+    }
+    // Check if first or last
+    if (this.actualPosition === (-295)) {
+      this.actualPosition = 5;
+    } else if (this.actualPosition === 105) {
+      this.actualPosition = -195;
+    }
+    // Update position
+    firstBaby.style.marginLeft = this.actualPosition + 'vw';
   }
 
 }
