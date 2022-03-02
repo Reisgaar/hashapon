14c53eadc625b52d51ab11f49ac0aca8df096474
Asiergaar <asier.garcia@nexxyolabs.com>
2022-03-02 10:36:28 +0100

    set reroute if wallet not connected

--- src/app/pages/gachapon/gachapon.component.ts
+++ src/app/pages/gachapon/gachapon.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { UtilsService } from 'src/app/shared/services/utils.service';
 
 @Component({
@@ -10,16 +10,12 @@ import { UtilsService } from 'src/app/shared/services/utils.service';
 export class GachaponComponent implements OnInit {
 
   constructor(
-    private activatedRoute: ActivatedRoute,
     private router: Router,
     private utilsService: UtilsService
-  ) {
-    this.activatedRoute.queryParams.subscribe(params => {
-      console.log(params['warning']);
-    });
-  }
+  ) { }
 
   ngOnInit(): void {
+    if (!this.utilsService.walletIsConnected) { this.router.navigate(['home']); }
     this.utilsService.changeActiveButton('button-gachapon');
   }
 
