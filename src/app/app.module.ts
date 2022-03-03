05b114de5d299866aec8d0c0762079f1b5219b4b
Asiergaar <asier.garcia@nexxyolabs.com>
2022-03-03 10:09:17 +0100

    create timer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { AnimalStatsComponent } from './pages/animal-stats/animal-stats.componen
 import { LobbyComponent } from './pages/lobby/lobby.component';
 import { BattleComponent } from './pages/battle/battle.component';
 import { HomeComponent } from './pages/home/home.component';
+import { TimerComponent } from './pages/timer/timer.component';
 
 
 @NgModule({
@@ -42,6 +43,7 @@ import { HomeComponent } from './pages/home/home.component';
     LobbyComponent,
     BattleComponent,
     HomeComponent,
+    TimerComponent,
   ],
   imports: [
     BrowserModule,
