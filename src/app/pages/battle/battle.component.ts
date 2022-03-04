e588960d627807bd4e895a154a42434c99a88f26
Asiergaar <asier.garcia@nexxyolabs.com>
2022-03-04 11:10:16 +0100

    redirect on wallet not connected

--- src/app/pages/battle/battle.component.ts
+++ src/app/pages/battle/battle.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UtilsService } from 'src/app/shared/services/utils.service';
 
 @Component({
   selector: 'app-battle',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BattleComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private utilsService: UtilsService
+  ) { }
 
   ngOnInit(): void {
+    if (!this.utilsService.walletIsConnected) { this.router.navigate(['home']); }
   }
 
 }
--- src/app/pages/lobby/lobby.component.ts
+++ src/app/pages/lobby/lobby.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UtilsService } from 'src/app/shared/services/utils.service';
 
 @Component({
   selector: 'app-lobby',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LobbyComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private utilsService: UtilsService
+  ) { }
 
   ngOnInit(): void {
+    if (!this.utilsService.walletIsConnected) { this.router.navigate(['home']); }
   }
 
 }
