81520a6ace3260f5ac18fa2ad322f2994a429cf7
Asiergaar <asier.garcia@nexxyolabs.com>
2022-03-04 11:03:32 +0100

    delete unused import

--- src/app/pages/animal-list/animal-list.component.ts
+++ src/app/pages/animal-list/animal-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { AnimalStatsComponent } from '../animal-stats/animal-stats.component';
 import { egg, baby, young, adult } from '../../shared/data/animal-data';
 import { UtilsService } from 'src/app/shared/services/utils.service';
 import { UpgradeService } from 'src/app/shared/services/upgrade.service';
