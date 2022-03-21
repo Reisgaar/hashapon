ed06878bd611a17e08ec80cbb41105fb5d8c01b2
Asiergaar <asier.garcia@nexxyolabs.com>
2022-03-21 13:01:12 +0100

    add functionality to service

--- src/app/shared/services/buy.service.ts
+++ src/app/shared/services/buy.service.ts
@@ -1,13 +1,31 @@
 import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { egg } from '../data/animal-data';
+import { UtilsService } from './utils.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class BuyService {
 
-  constructor() { }
+  constructor(
+    private utilsService: UtilsService,
+    private matDialog: MatDialog
+  ) { }
 
-  buyGachapon(): void {
-    console.log('Buy new gachapon');
+  async buyGachapon(): Promise<void> {
+    this.utilsService.openWaitForGachapon();
+
+    // Add functionality to buy egg
+    await this.getEgg(5000);
+
+    // Close dialog and open detail
+    this.matDialog.closeAll();
+    this.utilsService.openStats(egg[4]);
+  }
+
+  getEgg(ms: number): Promise<any> {
+    return new Promise( resolve => setTimeout(resolve, ms) );
   }
 }
