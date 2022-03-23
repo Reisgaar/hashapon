4ec70634070a347ff6704ea3608a55ddfc1234c4
Asiergaar <asier.garcia@nexxyolabs.com>
2022-03-23 15:13:20 +0100

    simulate opponent search

--- src/app/pages/popUp/searching-opponent/searching-opponent.component.ts
+++ src/app/pages/popUp/searching-opponent/searching-opponent.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-searching-opponent',
@@ -12,12 +12,24 @@ export class SearchingOpponentComponent implements OnInit {
   opponent: any;
 
   constructor(
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private dialogRef: MatDialogRef<SearchingOpponentComponent>
   ) {
     this.fighter = data.animal;
   }
 
   ngOnInit(): void {
+    this.simulateOpponentSearch();
+  }
+
+  async simulateOpponentSearch(): Promise<void> {
+    await this.getOpponent(5000).then(() => {
+      this.dialogRef.close();
+    });
+  }
+
+  getOpponent(ms: number): Promise<any> {
+    return new Promise( resolve => setTimeout(resolve, ms) );
   }
 
 }
