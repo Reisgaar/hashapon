d8820d72b77ea8969dd010b2d6c77f9fe765b6c8
Asiergaar <asier.garcia@nexxyolabs.com>
2022-03-23 12:26:04 +0100

    create opponent finder dialog

--- src/app/pages/battle/battle.component.ts
+++ src/app/pages/battle/battle.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { UtilsService } from 'src/app/shared/services/utils.service';
+import { SearchingOpponentComponent } from '../popUp/searching-opponent/searching-opponent.component';
 
 @Component({
   selector: 'app-battle',
@@ -12,7 +14,9 @@ export class BattleComponent implements OnInit {
   fighter: any;
 
   constructor(
+    public dialogRef: MatDialogRef<BattleComponent>,
     private router: Router,
+    public dialog: MatDialog,
     private utilsService: UtilsService
   ) {
     let navigation = this.router.getCurrentNavigation();
@@ -24,7 +28,26 @@ export class BattleComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    if (!this.utilsService.walletIsConnected) { this.router.navigate(['home']); }
+    if (!this.utilsService.walletIsConnected || this.fighter === null) {
+      this.router.navigate(['home']);
+    } else {
+      this.openSearchDialog();
+    }
+  }
+
+  openSearchDialog(): void {
+    const dialogRef = this.dialog.open(SearchingOpponentComponent, {
+      panelClass: 'waiting-opponent-dialog-container',
+      disableClose: true,
+      data: { animal: this.fighter }
+    });
+    //document.getElementById('selectFighter').style.visibility = 'hidden';
+    document.getElementById('gamescreen').style.filter = 'blur(5px)';
+
+    dialogRef.afterClosed().subscribe(result => {
+      //document.getElementById('selectFighter').style.visibility = 'visible';
+      document.getElementById('gamescreen').style.filter = 'unset';
+    });
   }
 
 }
--- src/app/pages/popUp/searching-opponent/searching-opponent.component.ts
+++ src/app/pages/popUp/searching-opponent/searching-opponent.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-searching-opponent',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchingOpponentComponent implements OnInit {
 
-  constructor() { }
+  fighter: any;
+  opponent: any;
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    this.fighter = data.animal;
+  }
 
   ngOnInit(): void {
   }
