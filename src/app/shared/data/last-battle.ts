50b24ca2ead4709e9fab2cded29203e10b1c5fed
Asiergaar <asier.garcia@nexxyolabs.com>
2022-03-24 14:32:15 +0100

    create battle data

---
+++ src/app/shared/data/last-battle.ts
@@ -0,0 +1,88 @@
+export let battle = {
+  creature1: 1,
+  creature2: 2,
+  winner: 2,
+  log: {
+      c1dice: 8,
+      c2dice: 4,
+      assaults: [
+          {
+              round: 0,
+              c1life: 30,
+              c1result: 4,
+              c2life: 40,
+              c2result: 2
+          },
+          {
+              round: 1,
+              c1life: 28,
+              c1result: 3,
+              c2life: 36,
+              c2result: 4
+          },
+          {
+              round: 2,
+              c1life: 24,
+              c1result: 6,
+              c2life: 33,
+              c2result: 2
+          },
+          {
+              round: 3,
+              c1life: 22,
+              c1result: 1,
+              c2life: 27,
+              c2result: 4
+          },
+          {
+              round: 4,
+              c1life: 18,
+              c1result: 1,
+              c2life: 26,
+              c2result: 2
+          },
+          {
+              round: 5,
+              c1life: 16,
+              c1result: 4,
+              c2life: 25,
+              c2result: 2
+          },
+          {
+              round: 6,
+              c1life: 14,
+              c1result: 2,
+              c2life: 21,
+              c2result: 3
+          },
+          {
+              round: 7,
+              c1life: 11,
+              c1result: 6,
+              c2life: 19,
+              c2result: 4
+          },
+          {
+              round: 8,
+              c1life: 7,
+              c1result: 2,
+              c2life: 13,
+              c2result: 1
+          },
+          {
+              round: 9,
+              c1life: 6,
+              c1result: 4,
+              c2life: 11,
+              c2result: 2
+          },
+          {
+              round: 10,
+              c1life: 4,
+              c1result: 5,
+              c2life: 7,
+              c2result: 4
+          }
+      ]
+  }
+}
