9c3434fdb347ee34f91776889c1adee567a0f5a9
Asiergaar <asier.garcia@nexxyolabs.com>
2022-03-28 13:32:07 +0200

    fix getwallet method

--- src/app/shared/services/connection/nft.service.ts
+++ src/app/shared/services/connection/nft.service.ts
@@ -12,22 +12,36 @@ export class NftService {
     private connectionService: ConnectionService
   ) { }
 
-  async getWalletNft(wallet: any): Promise<any> {
+
+
+  async getWalletNft(): Promise<any> {
+    const wallet = this.connectionService.getWalletAddress();
     const contract = new this.connectionService.web3js.eth.Contract(GachaponERC721Abi.abi, GachaponERC721Abi.address);
-    console.log(wallet);
     contract.defaultAccount = wallet;
     const nftBalance = await contract.methods.balanceOf(wallet).call();
-    console.log(nftBalance);
+    egg.splice(0, egg.length);
 
     for (let i = 0; i < nftBalance; i++) {
-      const tokenId = await contract.methods.tokenOfOwnerByIndex(wallet, i).call();
-      let tokenMetadataURI = await contract.methods.tokenURI(tokenId).call();
-      if (tokenMetadataURI.startsWith('ipfs://')) {
-        tokenMetadataURI = 'https://ipfs.io/ipfs/' + tokenMetadataURI.split('ipfs://')[1];
+      let tokenMetadata: any;
+      try {
+        const tokenId = await contract.methods.tokenOfOwnerByIndex(wallet, i).call();
+        let tokenMetadataURI = await contract.methods.tokenURI(tokenId).call();
+        tokenMetadataURI = this.setUrlIfIpfs(tokenMetadataURI);
+        tokenMetadata = await fetch(tokenMetadataURI).then ((response) => response.json());
+      } catch (error) {
+        tokenMetadata = {image: 'waiting'};
       }
-      const tokenMetadata = await fetch(tokenMetadataURI).then ((response) => response.json());
       egg.push(tokenMetadata);
       console.log(tokenMetadata);
     }
+    console.log(egg);
+  }
+
+  setUrlIfIpfs(url: string): string {
+    let tokenMetadataURI = url;
+    if (tokenMetadataURI.startsWith('ipfs://')) {
+      tokenMetadataURI = 'https://ipfs.io/ipfs/' + tokenMetadataURI.split('ipfs://')[1];
+    }
+    return url;
   }
 }
