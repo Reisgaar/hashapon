68363225232ee463ae69b10f845428626f117725
Asiergaar <asier.garcia@nexxyolabs.com>
2022-03-31 14:16:26 +0200

    change upgrade funtions individually

--- src/app/shared/services/contracts/upgrade.service.ts
+++ src/app/shared/services/contracts/upgrade.service.ts
@@ -8,23 +8,44 @@ export class UpgradeService {
 
   constructor() { }
 
-  startUpgrade(event: any, animal: any): void {
-    console.log('Start ' + this.getStage(animal) + ' upgrade');
+  // START UPGRADES
+
+  upgradeEggToChild(event: any, animal: any): void {
+    console.log('Start egg upgrade');
     event.stopPropagation();
   }
+
+  upgradeChildToYoung(event: any, animal: any): void {
+    console.log('Start child upgrade');
+    event.stopPropagation();
+  }
+
+  upgradeYoungToAdult(event: any, animal: any): void {
+    console.log('Start young upgrade');
+    event.stopPropagation();
+  }
+
+  upgradeAdultStat(event: any, animal: any): void {
+    console.log('Start stat upgrade');
+    event.stopPropagation();
+  }
+
+  upgradeStat(event: any, animal: any, stat: string): void {
+    console.log('Upgrade ' + animal.name + '\'s ' + stat);
+    event.stopPropagation();
+  }
+
+  // FORWARD UPGRADES
 
   forwardUpgrade(event: any, animal: any): void {
     console.log('Forward ' + this.getStage(animal) + ' upgrade');
     event.stopPropagation();
   }
 
+  // FINISH UPGRADES
+
   finishUpgrade(event: any, animal: any): void {
     console.log('Finish ' + this.getStage(animal) + ' upgrade');
-    event.stopPropagation();
-  }
-
-  upgradeStat(event: any, animal: any, stat: string): void {
-    console.log('Upgrade ' + animal.name + '\'s ' + stat);
     event.stopPropagation();
   }
 
@@ -34,6 +55,7 @@ export class UpgradeService {
       case 0: stage = 'egg'; break;
       case 1: stage = 'baby'; break;
       case 2: stage = 'young'; break;
+      case 3: stage = 'adult'; break;
       default: stage = ''; break;
     }
     return stage;
